7fe1cfa91355952ad5cfc11c9e61f304881dd51f
Charles Yiu <mike.flynn.abbe@gmail.com>
2021-09-25 13:41:40 +0800

    script changes

--- repo.js
+++ repo.js
@@ -1,7 +1,5 @@
-import {readFile} from fs
 let repoDiv = document.getElementById("repo")
-
-readFile("repoList.txt", (err, data) => {
+FileSystem.readFile("repoList.txt", (err, data) => {
     if (err) throw err
     console.log(data)
     repoDiv.innerHTML = data
