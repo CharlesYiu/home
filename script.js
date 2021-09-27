a6dc05ac52a7e6081c376aa79a89ffd5f44edd7a
Charles Yiu <charlesyiu.w@gmail.com>
2021-09-27 17:06:24 +0800

    Updated footer

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+document.getElementById('normalFooter').hidden = true
 fetch("https://raw.githubusercontent.com/charlesyiu/charlesyiu/main/README.md")
     .then(function (response) {
         return response.text()
